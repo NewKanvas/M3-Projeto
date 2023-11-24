1b6029739ac5540254337dc647746d9b634c1ee6
NewKanvas <cassioramaca@gmail.com>
2023-11-24 16:02:42 -0300

    update

--- SQL/funcoes.sql
+++ SQL/funcoes.sql
@@ -1,7 +1,6 @@
+-- C:\xampp\mysql\data\nomedobanco
 
 -- Função para adcionar alunos de acordo com a tabela csv:
--- C:\xampp\mysql\data\nomedobanco
-use resilia;
 LOAD DATA INFILE '\alunos.csv'
 REPLACE INTO TABLE alunos
 FIELDS TERMINATED BY ','
@@ -9,3 +8,9 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+-- Ver a tabela
+SELECT * FROM alunos;
+
+-- Ver filtrado
+
+SELECT * FROM alunos WHERE matricula LIKE '%4';
