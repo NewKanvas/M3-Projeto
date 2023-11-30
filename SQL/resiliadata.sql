5534da196c9e2fba347fff19075223d6ed2c76e8
NewKanvas <cassioramaca@gmail.com>
2023-11-30 16:54:56 -0300

    Create resiliadata.sql

---
+++ SQL/resiliadata.sql
@@ -0,0 +1,139 @@
+CREATE TABLE senac (
+unidade VARCHAR(100) PRIMARY KEY NOT NULL,
+endereco VARCHAR(250) NOT NULL);
+
+CREATE TABLE aluno (
+nome VARCHAR(200) NOT NULL,
+cpf INT(20) PRIMARY KEY NOT NULL,
+idade INT(3) NOT NULL,
+genero VARCHAR(100) NOT NULL,
+email VARCHAR(250) NOT NULL,
+senha VARCHAR(100) NOT NULL);
+
+CREATE TABLE matricula (
+matricula INT(50) PRIMARY KEY NOT NULL,
+unidade_fk VARCHAR(100) NOT NULL,
+curso_fk VARCHAR(200) NOT NULL,
+status VARCHAR(100) NOT NULL,
+cpf_aluno_fk INT(20) NOT NULL);
+
+CREATE TABLE curso (
+curso VARCHAR(200) PRIMARY KEY NOT NULL,
+duracao VARCHAR(100) NOT NULL,
+inicio DATE NOT NULL,
+fim DATE NOT NULL);
+
+CREATE TABLE turma (
+curso_fk VARCHAR(200) NOT NULL,
+unidade_fk VARCHAR(100) NOT NULL,
+turma_pk INT(3) PRIMARY KEY NOT NULL,
+sala INT(3) NOT NULL,
+computador INT(34) NOT NULL);
+
+CREATE TABLE monitor (
+cpf_monitor INT(11) PRIMARY KEY NOT NULL,
+nome VARCHAR(200) NOT NULL,
+idade INT(3) NOT NULL,
+genero VARCHAR(100) NOT NULL,
+email VARCHAR(250) NOT NULL,
+senha VARCHAR(250) NOT NULL);
+
+CREATE TABLE facilitador (
+cpf_facilitador INT(11) PRIMARY KEY NOT NULL,
+nome VARCHAR NOT NULL DEFAULT '250',
+idade INT(3) NOT NULL,
+genero VARCHAR(100) NOT NULL,
+especializacao VARCHAR(200) NOT NULL,
+email VARCHAR(250) NOT NULL,
+senha CHAR(250) NOT NULL);
+
+CREATE TABLE modulo (
+cpf_facilitador INT(11) NOT NULL,
+curso_fk VARCHAR(200) NOT NULL,
+modulo INT(10) PRIMARY KEY NOT NULL,
+avaliacao INT(3),
+cpf_monitor INT(11) NOT NULL);
+
+CREATE TABLE presenca (
+id_presenca INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+matricula_fk INTEGER(100) NOT NULL,
+aula_soft DATE,
+aula_hard DATE,
+cpf_fk VARCHAR(200) NOT NULL,
+presenca_aluno_facilitador_fk BOOLEAN NOT NULL,
+presenca_aluno_monitor_fk BOOLEAN NOT NULL,
+login_computador INT(250) NOT NULL);
+
+CREATE TABLE presenca_aluno_monitor  (
+presenca_aluno_monitor_fk BOOLEAN PRIMARY KEY NOT NULL,
+data DATE NOT NULL,
+modulo_fk INT(10) NOT NULL,
+cpf_monitor INT(11) NOT NULL,
+turma_fk VARCHAR(200) NOT NULL);
+
+CREATE TABLE presenca_aluno_facilitador (
+presenca_aluno_facilitador BOOLEAN PRIMARY KEY NOT NULL,
+data DATE NOT NULL,
+cpf_facilitador INT(11) NOT NULL,
+modulo INT(10) NOT NULL,
+turma_fk VARCHAR(200) NOT NULL);
+
+CREATE TABLE coordenação_senac (
+id_cordenacao INT(10) PRIMARY KEY NOT NULL,
+supervisor_tecnico VARCHAR(200) NOT NULL,
+coordenador VARCHAR(200) NOT NULL,
+turma_fk INT(10) NOT NULL,
+unidade_fk VARCHAR(100) NOT NULL);
+
+-- Matricula
+
+ALTER TABLE matricula ADD CONSTRAINT matricula_unidade_fk_senac_unidade FOREIGN KEY (unidade_fk) REFERENCES senac(unidade);
+ALTER TABLE matricula ADD CONSTRAINT matricula_curso_fk_curso_curso FOREIGN KEY (curso_fk) REFERENCES curso(curso);
+ALTER TABLE matricula ADD CONSTRAINT matricula_cpf_aluno_fk_aluno_cpf FOREIGN KEY (cpf_aluno_fk) REFERENCES aluno(cpf);
+
+-- Turma
+
+ALTER TABLE turma ADD CONSTRAINT turma_curso_fk_curso_curso FOREIGN KEY (curso_fk) REFERENCES curso(curso);
+ALTER TABLE turma ADD CONSTRAINT turma_unidade_fk_senac_unidade FOREIGN KEY (unidade_fk) REFERENCES senac(unidade);
+
+-- Modulo
+ 
+ALTER TABLE modulo 
+FOREIGN KEY (cpf_facilitador) REFERENCES facilitador(cpf_facilitador);
+
+ALTER TABLE modulo 
+FOREIGN KEY (curso_fk) REFERENCES curso(curso);
+
+ALTER TABLE modulo 
+FOREIGN KEY (cpf_monitor) REFERENCES monitor(cpf_monitor);
+
+-- Presença
+
+ALTER TABLE presenca 
+FOREIGN KEY (cpf_fk) 
+REFERENCES aluno(nome);
+
+ALTER TABLE presenca 
+FOREIGN KEY (presenca_aluno_facilitador_fk) 
+REFERENCES presenca_aluno_facilitador(presenca_aluno_facilitador);
+
+ALTER TABLE presenca 
+FOREIGN KEY (presenca_aluno_monitor_fk) 
+REFERENCES presenca_aluno_monitor (presenca_aluno_monitor_fk);
+
+-- Presença Aluno Monitor
+
+ALTER TABLE presenca_aluno_monitor  ADD CONSTRAINT presenca_aluno_monitor _modulo_fk_modulo_modulo FOREIGN KEY (modulo_fk) REFERENCES modulo(modulo);
+ALTER TABLE presenca_aluno_monitor  ADD CONSTRAINT presenca_aluno_monitor _cpf_monitor_monitor_cpf_monitor FOREIGN KEY (cpf_monitor) REFERENCES monitor(cpf_monitor);
+ALTER TABLE presenca_aluno_monitor  ADD CONSTRAINT presenca_aluno_monitor _turma_fk_turma_curso_fk FOREIGN KEY (turma_fk) REFERENCES turma(curso_fk);
+
+-- Presença Aluno Monitor
+
+ALTER TABLE presenca_aluno_facilitador ADD CONSTRAINT presenca_aluno_facilitador_cpf_facilitador_facilitador_cpf_facilitador FOREIGN KEY (cpf_facilitador) REFERENCES facilitador(cpf_facilitador);
+ALTER TABLE presenca_aluno_facilitador ADD CONSTRAINT presenca_aluno_facilitador_modulo_modulo_modulo FOREIGN KEY (modulo) REFERENCES modulo(modulo);
+ALTER TABLE presenca_aluno_facilitador ADD CONSTRAINT presenca_aluno_facilitador_turma_fk_turma_curso_fk FOREIGN KEY (turma_fk) REFERENCES turma(curso_fk);
+
+-- Coordenação Senac
+
+ALTER TABLE coordenação_senac ADD CONSTRAINT coordenação_senac_turma_fk_turma_turma_pk FOREIGN KEY (turma_fk) REFERENCES turma(turma_pk);
+ALTER TABLE coordenação_senac ADD CONSTRAINT coordenação_senac_unidade_fk_senac_unidade FOREIGN KEY (unidade_fk) REFERENCES senac(unidade);
