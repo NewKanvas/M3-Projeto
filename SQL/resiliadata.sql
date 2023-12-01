6c08b3af512a5f997c70b549f57acf347d13cc93
NewKanvas <cassioramaca@gmail.com>
2023-12-01 16:46:56 -0300

    update

--- SQL/resiliadata.sql
+++ SQL/resiliadata.sql
@@ -95,23 +95,87 @@ id_disciplina INT(100) PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nome VARCHAR(200) NOT NULL,
 carga_horaria INT(100) NOT NULL);
 
-ALTER TABLE matricula ADD CONSTRAINT matricula_unidade_fk_instituicao_unidade FOREIGN KEY (unidade_fk) REFERENCES instituicao(unidade);
-ALTER TABLE matricula ADD CONSTRAINT matricula_id_curso_fk_curso_id_curso FOREIGN KEY (id_curso_fk) REFERENCES curso(id_curso);
-ALTER TABLE matricula ADD CONSTRAINT matricula_cpf_aluno_fk_aluno_cpf FOREIGN KEY (cpf_aluno_fk) REFERENCES aluno(cpf);
-ALTER TABLE turma ADD CONSTRAINT turma_curso_fk_curso_id_curso FOREIGN KEY (curso_fk) REFERENCES curso(id_curso);
-ALTER TABLE modulo ADD CONSTRAINT modulo_curso_fk_curso_id_curso FOREIGN KEY (curso_fk) REFERENCES curso(id_curso);
-ALTER TABLE modulo ADD CONSTRAINT modulo_id_disciplina_fk_disciplina_id_disciplina FOREIGN KEY (id_disciplina_fk) REFERENCES disciplina(id_disciplina);
-ALTER TABLE presenca ADD CONSTRAINT presenca_cpf_fk_aluno_cpf FOREIGN KEY (cpf_fk) REFERENCES aluno(cpf);
-ALTER TABLE presenca ADD CONSTRAINT presenca_id_aula_facilitador_fk_presenca_aluno_facilitador_id_aula_facilitador FOREIGN KEY (id_aula_facilitador_fk) REFERENCES presenca_aluno_facilitador(id_aula_facilitador);
-ALTER TABLE presenca ADD CONSTRAINT presenca_id_aula_monitor_fk_presenca_aluno_monitor _id_aula_monitor_fk FOREIGN KEY (id_aula_monitor_fk) REFERENCES presenca_aluno_monitor (id_aula_monitor_fk);
-ALTER TABLE presenca_aluno_monitor  ADD CONSTRAINT presenca_aluno_monitor _modulo_fk_modulo_id_modulo FOREIGN KEY (modulo_fk) REFERENCES modulo(id_modulo);
-ALTER TABLE presenca_aluno_monitor  ADD CONSTRAINT presenca_aluno_monitor _turma_fk_turma_curso_fk FOREIGN KEY (turma_fk) REFERENCES turma(curso_fk);
-ALTER TABLE presenca_aluno_monitor  ADD CONSTRAINT presenca_aluno_monitor _cpf_monitor_monitor_cpf_monitor FOREIGN KEY (cpf_monitor) REFERENCES monitor(cpf_monitor);
-ALTER TABLE presenca_aluno_facilitador ADD CONSTRAINT presenca_aluno_facilitador_cpf_facilitador_facilitador_cpf_facilitador FOREIGN KEY (cpf_facilitador) REFERENCES facilitador(cpf_facilitador);
-ALTER TABLE presenca_aluno_facilitador ADD CONSTRAINT presenca_aluno_facilitador_modulo_modulo_id_modulo FOREIGN KEY (modulo) REFERENCES modulo(id_modulo);
-ALTER TABLE presenca_aluno_facilitador ADD CONSTRAINT presenca_aluno_facilitador_turma_fk_turma_curso_fk FOREIGN KEY (turma_fk) REFERENCES turma(curso_fk);
-ALTER TABLE avaliacao ADD CONSTRAINT avaliacao_cpf_fk_aluno_cpf FOREIGN KEY (cpf_fk) REFERENCES aluno(cpf);
-ALTER TABLE avaliacao ADD CONSTRAINT avaliacao_modulo_fk_modulo_id_modulo FOREIGN KEY (modulo_fk) REFERENCES modulo(id_modulo);
-ALTER TABLE avaliacao ADD CONSTRAINT avaliacao_id_disciplina_fk_disciplina_id_disciplina FOREIGN KEY (id_disciplina_fk) REFERENCES disciplina(id_disciplina);
-ALTER TABLE avaliacao ADD CONSTRAINT avaliacao_nome_modulo_pk_modulo_id_modulo FOREIGN KEY (nome_modulo_pk) REFERENCES modulo(id_modulo);
+-- Adicionando chaves estrangeiras
 
+-- Matrícula
+ALTER TABLE matricula 
+  ADD CONSTRAINT fk_matricula_unidade 
+  FOREIGN KEY (unidade_fk) REFERENCES instituicao(unidade);
+
+ALTER TABLE matricula 
+  ADD CONSTRAINT fk_matricula_curso 
+  FOREIGN KEY (id_curso_fk) REFERENCES curso(id_curso);
+
+ALTER TABLE matricula 
+  ADD CONSTRAINT fk_matricula_aluno 
+  FOREIGN KEY (cpf_aluno_fk) REFERENCES aluno(cpf);
+
+-- Turma
+ALTER TABLE turma 
+  ADD CONSTRAINT fk_turma_curso 
+  FOREIGN KEY (curso_fk) REFERENCES curso(id_curso);
+
+-- Módulo
+ALTER TABLE modulo 
+  ADD CONSTRAINT fk_modulo_curso 
+  FOREIGN KEY (curso_fk) REFERENCES curso(id_curso);
+
+ALTER TABLE modulo 
+  ADD CONSTRAINT fk_modulo_disciplina 
+  FOREIGN KEY (id_disciplina_fk) REFERENCES disciplina(id_disciplina);
+
+-- Presença
+ALTER TABLE presenca 
+  ADD CONSTRAINT fk_presenca_aluno 
+  FOREIGN KEY (cpf_fk) REFERENCES aluno(cpf);
+
+ALTER TABLE presenca 
+  ADD CONSTRAINT fk_presenca_facilitador 
+  FOREIGN KEY (id_aula_facilitador_fk) REFERENCES presenca_aluno_facilitador(id_aula_facilitador);
+
+ALTER TABLE presenca 
+  ADD CONSTRAINT fk_presenca_monitor 
+  FOREIGN KEY (id_aula_monitor_fk) REFERENCES presenca_aluno_monitor(id_aula_monitor_fk);
+
+-- Presença Aluno Monitor
+ALTER TABLE presenca_aluno_monitor  
+  ADD CONSTRAINT fk_presenca_aluno_monitor_modulo 
+  FOREIGN KEY (modulo_fk) REFERENCES modulo(id_modulo);
+
+ALTER TABLE presenca_aluno_monitor  
+  ADD CONSTRAINT fk_presenca_aluno_monitor_turma 
+  FOREIGN KEY (turma_fk) REFERENCES turma(curso_fk);
+
+ALTER TABLE presenca_aluno_monitor  
+  ADD CONSTRAINT fk_presenca_aluno_monitor_monitor 
+  FOREIGN KEY (cpf_monitor) REFERENCES monitor(cpf_monitor);
+
+-- Presença Aluno Facilitador
+ALTER TABLE presenca_aluno_facilitador 
+  ADD CONSTRAINT fk_presenca_aluno_facilitador_facilitador 
+  FOREIGN KEY (cpf_facilitador) REFERENCES facilitador(cpf_facilitador);
+
+ALTER TABLE presenca_aluno_facilitador 
+  ADD CONSTRAINT fk_presenca_aluno_facilitador_modulo 
+  FOREIGN KEY (modulo) REFERENCES modulo(id_modulo);
+
+ALTER TABLE presenca_aluno_facilitador 
+  ADD CONSTRAINT fk_presenca_aluno_facilitador_turma 
+  FOREIGN KEY (turma_fk) REFERENCES turma(curso_fk);
+
+-- Avaliação
+ALTER TABLE avaliacao 
+  ADD CONSTRAINT fk_avaliacao_aluno 
+  FOREIGN KEY (cpf_fk) REFERENCES aluno(cpf);
+
+ALTER TABLE avaliacao 
+  ADD CONSTRAINT fk_avaliacao_modulo 
+  FOREIGN KEY (modulo_fk) REFERENCES modulo(id_modulo);
+
+ALTER TABLE avaliacao 
+  ADD CONSTRAINT fk_avaliacao_disciplina 
+  FOREIGN KEY (id_disciplina_fk) REFERENCES disciplina(id_disciplina);
+
+ALTER TABLE avaliacao 
+  ADD CONSTRAINT fk_avaliacao_modulo_nome 
+  FOREIGN KEY (nome_modulo_pk) REFERENCES modulo(id_modulo);
