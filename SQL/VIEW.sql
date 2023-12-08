d8544ca8545770f560e8250475a9767fd3f029e6
NewKanvas <cassioramaca@gmail.com>
2023-12-07 22:05:57 -0300

    Update VIEW.sql

--- SQL/VIEW.sql
+++ SQL/VIEW.sql
@@ -9,6 +9,7 @@ atributos;
 4. Criar scripts SQL para inserção dos dados nas tabelas
 5. Executar consultas para gerar informações estratégicas para a área de ensino da Resilia
 Detalhes do projeto:
+
 ● Após a criação do banco de dados, você e sua equipe deverão inserir dados para teste do banco
 de dados. Vocês deverão executar as consultas abaixo e apresentar seus resultados:
 1. Selecionar a quantidade total de estudantes cadastrados no banco;
@@ -21,7 +22,36 @@ e inserir um novo dado em uma tabela de log.
 SQL que combine pelo menos 3 tabelas.
 '''
 
--- Apenas teorico
+-- 1. Selecionar a quantidade total de estudantes cadastrados no banco;
 CREATE VIEW numero_alunos AS
-SELECT COUNT(aluno.id_aluno) 
-FROM aluno
+SELECT COUNT(DISTINCT aluno.cpf) AS total_alunos
+FROM aluno;
+
+SELECT * FROM numero_alunos;
+
+'''
+Como o CPF esta como BIGINT eu estou usando o DISTINCT para tentar contalo apenas uma vez (tem que testar ainda).
+'''
+
+
+-- 2. Selecionar quais pessoas facilitadoras atuam em mais de uma turma;
+
+CREATE VIEW facilitadores_maior2 AS
+SELECT cpf_facilitador, COUNT(DISTINCT turma_fk) AS total_turmas
+FROM presenca_aluno_facilitador
+GROUP BY cpf_facilitador
+HAVING COUNT(DISTINCT turma_fk) > 1;
+
+-- 3. Crie uma view que selecione a porcentagem de estudantes com status de evasão agrupados por turma;
+-- Aluno -> Avaliaçao -> Diciplina - Modulo -> Curso -> Turma
+
+-- # Tabela de Status em Geral (Ideia para 5 Pergunta) depois filtra para somente os Evadidos
+CREATE VIEW status_alunos AS
+SELECT 
+    aluno.nome,
+    aluno.cpf, 
+    turma.nome_da_turma, 
+    curso.nome_curso, 
+    diciplina.nome, 
+    avaliacao.nota, 
+    avaliacao.status
