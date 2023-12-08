99e64cf3f46715aaa2f7c5da59e0202e74a0dffd
NewKanvas <cassioramaca@gmail.com>
2023-12-08 16:57:33 -0300

    up

--- SQL/Inserts/CURSO.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO curso (id_curso, nome_curso, duracao) VALUES
-(1, "Formação em Análise de Dados", "6 meses"),
-(2, "Segurança da Informação", "6 meses"),
-(3, "Sistemas Embarcados", "6 meses"),
-(4, "Gestão de Redes de Computadores", "6 meses")
