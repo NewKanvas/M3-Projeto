134dce460f912ad004b0ca4488c171893436cab2
NewKanvas <cassioramaca@gmail.com>
2023-12-08 15:33:52 -0300

    update

--- SQL/trigger.sql
+++ SQL/trigger.sql
@@ -1,6 +1,7 @@
 -- 4. Crie um trigger para ser disparado quando o atributo status de um estudante for atualizado
 -- e inserir um novo dado em uma tabela de log.
 
+--Criar 
 CREATE TABLE IF NOT EXISTS log_status_aluno (
     id_log INT AUTO_INCREMENT PRIMARY KEY,
     cpf_aluno BIGINT(11),
