81f841112e53f8684a5dc92c13f2858174a4acd6
NewKanvas <cassioramaca@gmail.com>
2023-12-08 21:04:19 -0300

    Update resiliadata.sql

--- SQL/resiliadata.sql
+++ SQL/resiliadata.sql
@@ -5,95 +5,113 @@ CREATE DATABASE RESILIADATA;
 USE RESILIADATA;
 
 CREATE TABLE instituicao (
-`unidade` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`endereco` VARCHAR(250) NOT NULL,
-`nome_unidade` VARCHAR(200) NOT NULL);
+  `unidade` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `endereco` VARCHAR(250) NOT NULL,
+  `nome_unidade` VARCHAR(200) NOT NULL);
 
 CREATE TABLE aluno (
-`nome` VARCHAR(200) NOT NULL,
-`cpf` BIGINT(11) PRIMARY KEY NOT NULL,
-`idade` INT(3) NOT NULL,
-`genero` VARCHAR(100) NOT NULL,
-`email` VARCHAR(250) NOT NULL);
+  `nome` VARCHAR(200) NOT NULL,
+  `cpf` BIGINT(11) PRIMARY KEY NOT NULL,
+  `idade` INT(3) NOT NULL,
+  `genero` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(250) NOT NULL);
 
 CREATE TABLE matricula (
-`matricula` INT(11) PRIMARY KEY NOT NULL,
-`unidade_fk` INT(11) NOT NULL,
-`id_curso_fk` INT(11) NOT NULL,
-`status` VARCHAR(100) NOT NULL,
-`cpf_aluno_fk`  BIGINT(11) NOT NULL);
+  `matricula` INT(11) PRIMARY KEY NOT NULL,
+  `unidade_fk` INT(11) NOT NULL,
+  `id_curso_fk` INT(11) NOT NULL,
+  `status` VARCHAR(100) NOT NULL,
+  `cpf_aluno_fk`  BIGINT(11) NOT NULL);
 
 CREATE TABLE curso (
-`id_curso` INT(11) PRIMARY KEY NOT NULL,
-`nome_curso` VARCHAR(200) NOT NULL,
-`duracao` VARCHAR(100) NOT NULL,
-`inicio` DATE NOT NULL,
-`fim` DATE NOT NULL);
+  `id_curso` INT(11) PRIMARY KEY NOT NULL,
+  `nome_curso` VARCHAR(200) NOT NULL,
+  `duracao` VARCHAR(100) NOT NULL,
+  `total_dias` INT(11) NOT NULL);
 
 CREATE TABLE turma (
-`curso_fk` INT(11) NOT NULL,
-`turma_pk` INT(3) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`sala` INT(3) NOT NULL,
-`computador` INT(34) NOT NULL);
+  `curso_fk` INT(11) NOT NULL,
+  `turma_pk` INT(3) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `sala` INT(3) NOT NULL);
 
 CREATE TABLE monitor (
-`cpf_monitor`  BIGINT(11) PRIMARY KEY NOT NULL,
-`nome` VARCHAR(200) NOT NULL,
-`idade` INT(3) NOT NULL,
-`genero` VARCHAR(100) NOT NULL,
-`email` VARCHAR(250) NOT NULL);
+  `cpf_monitor`  BIGINT(11) PRIMARY KEY NOT NULL,
+  `nome` VARCHAR(200) NOT NULL,
+  `idade` INT(3) NOT NULL,
+  `genero` VARCHAR(100) NOT NULL,
+  `email` VARCHAR(250) NOT NULL);
 
 CREATE TABLE facilitador (
-`cpf_facilitador`  BIGINT(11) PRIMARY KEY NOT NULL,
-`nome` VARCHAR(200),
-`idade` INT(3) NOT NULL,
-`genero` VARCHAR(100) NOT NULL,
-`especializacao` VARCHAR(200) NOT NULL,
-`email` VARCHAR(250) NOT NULL);
+  `cpf_facilitador`  BIGINT(11) PRIMARY KEY NOT NULL,
+  `nome` VARCHAR(200),
+  `idade` INT(3) NOT NULL,
+  `genero` VARCHAR(100) NOT NULL,
+  `especializacao` VARCHAR(200) NOT NULL,
+  `email` VARCHAR(250) NOT NULL);
 
 CREATE TABLE modulo (
-`curso_fk` INT(11) NOT NULL,
-`id_modulo` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`qt_aula` INT(11) NOT NULL,
-`carga_horaria` INT(11) NOT NULL,
-`id_disciplina_fk` INT(11) NOT NULL);
+  `curso_fk` INT(11) NOT NULL,
+  `id_modulo` INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `numero_modulo` INT(11) NOT NULL,
+  `qt_aula` INT(11) NOT NULL,
+  `carga_horaria` INT(11) NOT NULL,
+  `id_disciplina_fk` INT(11) NOT NULL);
 
 CREATE TABLE disciplina (
-`id_disciplina` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`nome` VARCHAR(200) NOT NULL,
-`carga_horaria` INT(11) NOT NULL);
+  `id_disciplina` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `nome` VARCHAR(200) NOT NULL,
+  `carga_horaria` INT(11) NOT NULL);
 
 CREATE TABLE presenca_aluno_monitor  (
-`id_aula_monitor_fk` INTEGER(200) PRIMARY KEY NOT NULL,
-`data` DATE NOT NULL,
-`modulo_fk` INT(11) NOT NULL,
-`turma_fk` INT(11) NOT NULL,
-`cpf_monitor`  BIGINT(11) NOT NULL);
+  `id_aula_monitor_fk` INTEGER(200) PRIMARY KEY NOT NULL,
+  `data` DATE NOT NULL,
+  `presenca_monitor` BOOLEAN NOT NULL DEFAULT 0,
+  `modulo_fk` INT(11) NOT NULL,
+  `turma_fk` INT(11) NOT NULL,
+  `cpf_monitor`  BIGINT(11) NOT NULL);
 
 CREATE TABLE presenca_aluno_facilitador (
-`id_aula_facilitador` INTEGER PRIMARY KEY NOT NULL,
-`data` DATE NOT NULL,
-`cpf_facilitador`  BIGINT(11) NOT NULL,
-`modulo` INT(10) NOT NULL,
-`turma_fk` INT(11) NOT NULL);
+  `id_aula_facilitador` INTEGER PRIMARY KEY NOT NULL,
+  `data` DATE NOT NULL,
+  `cpf_facilitador`  BIGINT(11) NOT NULL,
+  `presenca_facilitador` BOOLEAN NOT NULL DEFAULT 0,
+  `modulo` INT(10) NOT NULL,
+  `turma_fk` INT(11) NOT NULL);
 
 CREATE TABLE presenca (
-`id_presenca` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
-`matricula_fk` INT(11) NOT NULL,
-`cpf_fk`  BIGINT(11) NOT NULL,
-`id_aula_facilitador_fk` INT(11) NOT NULL,
-`presenca_facilitador` INT(11) NOT NULL,
-`id_aula_monitor_fk` INT(11) NOT NULL,
-`presenca_modulo` INT(11) NOT NULL);
+  `id_presenca` INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `matricula_fk` INT(11) NOT NULL,
+  `cpf_fk` BIGINT(11) NOT NULL,
+  `data_fk` DATE NOT NULL,
+  `id_aula_facilitador_fk` INT(11) NOT NULL,
+  `presenca_facilitador_fk` BOOLEAN NOT NULL DEFAULT 0,
+  `id_aula_monitor_fk` INT(11) NOT NULL,
+  `presenca_monitor_fk` BOOLEAN NOT NULL DEFAULT 0,
+  `presenca_modulo` INT(11) NOT NULL,
+
+-- Adicionando chaves estrangeiras em presenca
+ALTER TABLE presenca
+  ADD CONSTRAINT fk_presenca_facilitador
+  FOREIGN KEY (id_aula_facilitador_fk) REFERENCES presenca_aluno_facilitador(id_aula_facilitador);
+
+ALTER TABLE presenca
+  ADD CONSTRAINT fk_presenca_monitor
+  FOREIGN KEY (id_aula_monitor_fk) REFERENCES presenca_aluno_monitor(id_aula_monitor_fk);
+
+UPDATE presenca
+SET presenca_modulo = CASE WHEN (
+    presenca_facilitador_fk = 1 AND
+    presenca_monitor_fk = 1
+) THEN 1 ELSE 0 END;
+
 
 CREATE TABLE avaliacao (
-`cpf_fk` BIGINT(11) NOT NULL,
-`modulo_fk` INT(10) NOT NULL,
-`nota` INT(11) NOT NULL,
-`status` VARCHAR(100) NOT NULL,
-`id_disciplina_fk` INT(11) NOT NULL,
-`nome_modulo_pk` INT NOT NULL);
-
+  `cpf_fk` BIGINT(11) NOT NULL,
+  `modulo_fk` INT(10) NOT NULL,
+  `nota` INT(11) NOT NULL,
+  `status` VARCHAR(100) NOT NULL,
+  `id_disciplina_fk` INT(11) NOT NULL,
+  `nome_modulo_pk` INT NOT NULL);
 
 
 -- Adicionando chaves estrangeiras
